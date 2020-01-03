548b350c91a0a3d52f8d832bbf02a0a80461603f
shuwang0000 <shuwang0000@gmail.com>
2020-01-04 06:37:03 +0800

    refactor(presets): remove jsx

--- presets/jsx.js
+++
@@ -1,9 +0,0 @@
-const { transformFile } = require('@babel/core')
-
-transformFile('./src/main.jsx', { presets: ['@babel/preset-env', '@babel/preset-react'] }, function (err, res) {
-    if (err) {
-        return console.log(err)
-    }
-    const { code } = res
-    console.log(code)
-})
--- presets/src/main.jsx
+++
@@ -1,4 +0,0 @@
-/** @jsx Didact.createElement */
-const getMsg = () => 'hello,jsx!'
-const msg = getMsg();
-< div > <span>{msg}</span></div>
